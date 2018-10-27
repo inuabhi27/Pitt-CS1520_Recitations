78e5d15558bce99454b8ac31f24a4bc24c1cd36b
debarundas1 <ded59@pitt.edu>
2018-10-27 00:51:32 -0400

    Add files via upload

---
+++ Recitation7/cookie.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+from flask import Flask, render_template, request, redirect, url_for, flash, make_response
+
+app = Flask(__name__)
+@app.route('/cookie/')
+def cookie():
+    res = make_response("Setting a cookie")
+    res.set_cookie('foo', 'bar', max_age=60*60*24*365*2)
+    return res
+
+@app.route('/delete-cookie/')
+def delete_cookie():
+    res = make_response("Cookie Removed")
+    res.set_cookie('foo', 'bar', max_age=0)
+    return res
+if __name__ == "__main__":
+    app.run()
+
+
+	
